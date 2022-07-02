1a49f6920a7770ab44f66906868eadb92e664218
Viridian <96497535+ViridianTelamon@users.noreply.github.com>
2022-07-02 15:59:16 -0400

    Add files via upload

---
+++ Jin San Discord Bot.py
@@ -0,0 +1,38 @@
+import discord
+import os
+import time
+import requests
+import json
+
+#Jin San Discord Bot.
+#By:  ViridianTelamon.
+
+client = discord.Client()
+
+token = "Put Discord Bot Token Here."
+
+#Get Quote Event.
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]["q"] + " -"+json_data[0]["a"]
+  return(quote)
+
+#On Ready Event.
+@client.event
+async def on_ready():
+  print("Bot Ready.")
+  time.sleep(1)
+  print("Logged In As:  {0.user}".format(client))
+
+#If The Bot Recieves A Message Event.
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith(":quote"):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+client.run(token)
